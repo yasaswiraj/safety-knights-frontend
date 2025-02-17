2d0cb3f633bc2b32cd887ef773883c1b08305ee1
yasaswiraj <yasaswirajmadari@gmail.com>
2025-02-17 15:03:58 -0500

    added sign up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-export const routes: Routes = [{ path: '', component: LoginComponent }];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'sign-up', component: SignUpComponent }
+];
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  signUpForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+    });
+    onLogin() {
+      console.log(this.signUpForm);
+    }
+}
