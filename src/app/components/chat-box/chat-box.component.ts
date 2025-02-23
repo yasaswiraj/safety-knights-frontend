09ac7a4d94c334191717d1171864961d42df650a
yasaswiraj <yasaswirajmadari@gmail.com>
2025-02-22 19:56:08 -0500

    added chat UI

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -1,11 +1,137 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat-box',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-box.component.html',
   styleUrl: './chat-box.component.css'
 })
 export class ChatBoxComponent {
+  chatUser   = {
+    name: 'Natasha',
+    avatar: 'https://randomuser.me/api/portraits/women/45.jpg' 
+  }
+  messages = [
+    {
+      id: 1,
+      sender: "Natasha Romanoff",
+      message: "Hey, I need some help here.",
+      time: "15:45",
+      isMe: false,
+    },
+    {
+      id: 2,
+      sender: "You",
+      message: "Sure! What do you need help with?",
+      time: "15:46",
+      isMe: true,
+    },
+    {
+      id: 3,
+      sender: "Natasha Romanoff",
+      message: "I think my login is not working.",
+      time: "15:47",
+      isMe: false,
+    },
+    {
+      id: 4,
+      sender: "You",
+      message: "Oh, I see. Did you forget your password?",
+      time: "15:48",
+      isMe: true,
+    },
+    {
+      id: 5,
+      sender: "Natasha Romanoff",
+      message: "No, I just reset it but it's still not working.",
+      time: "15:49",
+      isMe: false,
+    },
+    {
+      id: 6,
+      sender: "You",
+      message: "Okay, let me check on the backend.",
+      time: "15:50",
+      isMe: true,
+    },
+    {
+      id: 7,
+      sender: "Natasha Romanoff",
+      message: "Thanks, let me know what you find.",
+      time: "15:51",
+      isMe: false,
+    },
+    {
+      id: 8,
+      sender: "You",
+      message: "Sure, I'll keep you posted.",
+      time: "15:52",
+      isMe: true,
+    },
+    {
+      id: 9,
+      sender: "Natasha Romanoff",
+      message: "Great, thanks!",
+      time: "15:53",
+      isMe: false,
+    },
+    {
+      id: 10,
+      sender: "You",
+      message: "You're welcome.",
+      time: "15:54",
+      isMe: true,
+    },
+    {
+      id: 11,
+      sender: "Natasha Romanoff",
+      message: "Hey, how are you?",
+      time: "15:35",
+      isMe: false,
+    },
+    {
+      id: 12,
+      sender: "You",
+      message: "I'm good, thanks for asking.",
+      time: "15:36",
+      isMe: true,
+    },
+    {
+      id: 13,
+      sender: "Natasha Romanoff",
+      message: "That's good to hear.",
+      time: "15:37",
+      isMe: false,
+    },
+    {
+      id: 14,
+      sender: "You",
+      message: "Yeah, how about you?",
+      time: "15:38",
+      isMe: true,
+    },
+    {
+      id: 15,
+      sender: "Natasha Romanoff",
+      message: "I'm good too.",
+      time: "15:39",
+      isMe: false,
+    },
+    {
+      id: 16,
+      sender: "You",
+      message: "That's great.",
+      time: "15:40",
+      isMe: true,
+    },
+    {
+      id: 17,
+      sender: "Natasha Romanoff",
+      message: "Thanks, let me know what you find.",
+      time: "15:45",
+      isMe: false,
+    }
+  ].reverse();
 
 }
--- src/app/components/messages-list/messages-list.component.ts
+++ src/app/components/messages-list/messages-list.component.ts
@@ -12,22 +12,59 @@ export class MessagesListComponent {
   chats = [
     {
       id: 1,
-      user:{
-        name: 'Natasha',
-        avatar: 'https://randomuser.me/api/portraits/women/79.jpg' 
+      user: {
+        name: "Natasha Romanoff",
+        avatar: "https://randomuser.me/api/portraits/women/45.jpg",
       },
-      lastMessage: 'I need some help here',
-      time: '15:45',
+      lastMessage: "Thanks, let me know what you find.",
+      time: "15:45",
     },
     {
       id: 2,
-      user:{
-        name: 'Chris Hemsworth',
-        avatar: 'https://randomuser.me/api/portraits/men/81.jpg',
+      user: {
+        name: "Chris Hemsworth",
+        avatar: "https://randomuser.me/api/portraits/men/32.jpg",
       },
       lastMessage: "I can't find Loki again!",
-      time: '12:45',
-    }
+      time: "15:42",
+    },
+    {
+      id: 3,
+      user: {
+        name: "Ana De Armas",
+        avatar: "https://randomuser.me/api/portraits/women/30.jpg",
+      },
+      lastMessage: "Hey, how are you?",
+      time: "15:35",
+    },
+    {
+      id: 4,
+      user: {
+        name: "Peter Stark",
+        avatar: "https://randomuser.me/api/portraits/men/28.jpg",
+      },
+      lastMessage: "Help me update my name!",
+      time: "15:30",
+    },
+    {
+      id: 5,
+      user: {
+        name: "Voldemort",
+        avatar: "https://randomuser.me/api/portraits/men/99.jpg",
+      },
+      lastMessage: "Get me Potter!!!!",
+      time: "15:25",
+    },
+    {
+      id: 6,
+      user: {
+        name: "Barry Allen",
+        avatar: "https://randomuser.me/api/portraits/men/40.jpg",
+      },
+      lastMessage: "Speed force is dying again.",
+      time: "15:15",
+    },
   ];
+  
 
 }
