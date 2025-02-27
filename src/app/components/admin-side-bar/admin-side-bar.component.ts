2dc7360aebf37e78314a2bc7bce67330a75e0e12
yasaswiraj <yasaswirajmadari@gmail.com>
2025-02-26 22:11:29 -0500

    minor fixes

--- src/app/components/admin-side-bar/admin-side-bar.component.ts
+++ src/app/components/admin-side-bar/admin-side-bar.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-side-bar',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin-side-bar.component.html',
-  styleUrl: './admin-side-bar.component.css'
+  styleUrl: './admin-side-bar.component.css',
 })
 export class AdminSideBarComponent {
+  @Input() isSidebarOpen = true;
+  activeLabel = signal<string>('Dashboard');
+
+  isActive(label: string): boolean {
+    return this.activeLabel() === label;
+  }
+
+  setActiveLabel(label: string) {
+    this.activeLabel.set(label);
+  }
+
   menuItems = [
     { label: 'Dashboard', route: '/admin', icon: 'fas fa-home' },
     { label: 'All Users', route: '/admin/users-list', icon: 'fas fa-users' },
@@ -14,8 +27,10 @@ export class AdminSideBarComponent {
     { label: 'Bids', route: '/admin/bids-list', icon: 'fas fa-gavel' },
     { label: 'Inbox', route: '/admin/chat', icon: 'fas fa-envelope' },
     { label: 'Vetting', route: '/admin/vetting', icon: 'fas fa-circle-check' },
-    { label: 'Site Settings', route: '/admin/site-settings', icon: 'fas fa-cog' },
+    {
+      label: 'Site Settings',
+      route: '/admin/site-settings',
+      icon: 'fas fa-cog',
+    },
   ];
-
-  @Input() isSidebarOpen = true;
 }
