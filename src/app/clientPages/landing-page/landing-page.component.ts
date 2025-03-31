453064575712630be81202eeba70d01efd84cd83
yasaswiraj <yasaswirajmadari@gmail.com>
2025-03-31 17:45:35 -0400

    added an example in the landing page

--- src/app/clientPages/landing-page/landing-page.component.ts
+++ src/app/clientPages/landing-page/landing-page.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { HttpClient } from '@angular/common/http'; // Import HttpClient
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-landing-page',
@@ -15,7 +16,7 @@ export class LandingPageComponent implements OnInit {
   constructor(private http: HttpClient) {} // Inject HttpClient
 
   ngOnInit() {
-    this.http.get('http://localhost:8000') // Replace with your API endpoint
+    this.http.get(`${environment.apiUrl}`) // Replace with your API endpoint
       .subscribe(response => {
         console.log(response); // Log the response to the console
       });
