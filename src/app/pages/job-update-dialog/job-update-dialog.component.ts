1221ede7f8d24d72a8c1d0f32c68b29e6507685a
rakshathavasudev <rakshathavasudev@gmail.com>
2025-04-10 11:43:14 -0400

    closed status fix

--- src/app/pages/job-update-dialog/job-update-dialog.component.ts
+++ src/app/pages/job-update-dialog/job-update-dialog.component.ts
@@ -35,7 +35,7 @@ export class JobUpdateDialogComponent {
     switch (this.selectedStatus) {
       case 'in_progress':
         return `${environment.apiUrl}/consultant/update_active_to_in_progress/${jobId}`;
-      case 'completed':
+      case 'closed':
         return `${environment.apiUrl}/consultant/update_in_progress_to_closed/${jobId}`;
       case 'waiting':
         return `${environment.apiUrl}/consultant/update_in_progress_to_active/${jobId}`;
